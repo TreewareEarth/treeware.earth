70a7c745a40bf44ce85f7c0c45a94ab221f08525
James Mills <james@jamesmills.co.uk>
2020-01-12 12:51:36 +0400

    Initial marketing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,268 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<!doctype html>
+<html lang="en">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+<head>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
 
-            .full-height {
-                height: 100vh;
-            }
+    <title>Treeware, Treeware, software distribution supporting a sustainable future.</title>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PDB4V4F');</script>
+    <!-- End Google Tag Manager -->
 
-            .position-ref {
-                position: relative;
-            }
+</head>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+<body class="antialiased font-sans bg-white text-gray-900">
 
-            .content {
-                text-align: center;
-            }
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PDB4V4F"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
-            .title {
-                font-size: 84px;
-            }
+<main class="w-full">
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+    <!-- start header -->
+    <header class="w-full bg-transparent fixed top-0 left-0 z-50 px-4 sm:px-8 lg:px-16 xl:px-40 2xl:px-64 py-6"
+            id="navbar">
+        <nav class="flex items-center justify-between">
+            <div class="">
+                <a href='/fair-rate-mortgage/'>
+                    Treeware.Earth
+                </a>
+            </div>
+        </nav>
+    </header>
+    <!-- end header -->
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+    <!-- start hero section -->
+    <section class="relative bg-blue-100 px-4 sm:px-8 lg:px-16 xl:px-40 2xl:px-64 pt-32 pb-24 sm:pb-64 md:pt-40 md:pb-48 lg:pt-40 xl:pb-48 2xl:pt-56 2xl:pb-96 text-center sm:text-left">
+        <div>
+            <div class="relative w-full sm:w-2/3 lg:w-1/2 z-10">
+                <h1 class="text-3xl lg:text-4xl xl:text-5xl leading-tight font-bold text-yellow-800">Treeware</h1>
+                <p class="text-base leading-snug text-gray-700 mt-4 text-yellow-800 text-xl">Treeware is a style of software distribution similar to Postcardware, distributed by the author on the condition that users buy the author a tree.</p>
+                <a href="#" class="w-full block sm:inline-block sm:w-auto px-6 py-4 bg-green-600 text-white rounded-lg mt-8">Add your package</a>
+            </div>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+            <div class="w-full absolute bottom-0 right-0 text-center;" style="margin-bottom: -100px;z-index: 100;">
+                <img style="margin: auto;" src="/img/tree-grow-no-bg.png" alt="">
             </div>
         </div>
-    </body>
+    </section>
+    <!-- end hero section -->
+
+    <!-- start how it works -->
+    <section class="relative px-4 sm:px-8 lg:px-16 xl:px-40 2xl:px-64 py-20 text-center" style="background-color: #E5CDAD">
+        <div>
+            <h2 class="text-3xl leading-tight font-bold py-8 text-yellow-900">How Treeware Works?</h2>
+        </div>
+
+
+        <div class="flex flex-wrap overflow-hidden lg:-mx-2">
+
+
+            <div class="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/3">
+                <div class="rounded overflow-hidden shadow-lg bg-white">
+                    <img class="w-full" src="/img/tree_three.jpg" alt="Sunset in the mountains">
+                    <div class="px-6 py-4">
+                        <div class="font-bold text-xl mb-2">1. Opensource Package</div>
+                        <p class="text-gray-700 text-base">
+                            You have an Opensource package and you would like to help support a sustainable future. You continue to offer your package under your current licence (e.g. MIT).
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/3">
+                <div class="rounded overflow-hidden shadow-lg bg-white">
+                    <img class="w-full" src="/img/tree_three.jpg" alt="Sunset in the mountains">
+                    <div class="px-6 py-4">
+                        <div class="font-bold text-xl mb-2">2. Add Treeware licence</div>
+                        <p class="text-gray-700 text-base">
+                            Update your README.md file to include our Treeware "conditions of use" text. This will provide people with a way to buy trees for the Treeware Forrest on Offset.Earth.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/3">
+                <div class="rounded overflow-hidden shadow-lg bg-white">
+                    <img class="w-full" src="/img/tree_three.jpg" alt="Sunset in the mountains">
+                    <div class="px-6 py-4">
+                        <div class="font-bold text-xl mb-2">3. Watch our forrest grow!</div>
+                        <p class="text-gray-700 text-base">
+                            When people use your package we hope they will support you by buying the world a few trees. In turn you’ll help to accelerate our transition to a sustainable future.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+              {{--<div class="w-full overflow-hidden lg:my-2 lg:px-2 lg:w-1/3">--}}
+                {{--<img class="w-full" src="img/tree_one.jpg" alt="Sunset in the mountains">--}}
+                {{--<div class="px-6 py-4">--}}
+                    {{--<div class="font-bold text-xl mb-2">1. Opensource Package</div>--}}
+                    {{--<p class="text-gray-700 text-base">--}}
+                        {{--You have an Opensource package and you would like to help support a sustainable future. You can continue to offer your package under your current licence (e.g. MIT).--}}
+                    {{--</p>--}}
+                {{--</div>--}}
+
+
+
+            {{--<div class="w-full bg-white shadow-lg rounded-lg px-4 py-6 lg:p-8 md:mx-2 lg:mx-4 mt-4 md:mt-0">--}}
+                {{--<img src="images/icon-home-2.svg" alt="" class="mx-auto h-32">--}}
+                {{--<h4 class="text-xl font-bold leading-tight mt-8">2. Add Treeware licence</h4>--}}
+                {{--<p class="text-gray-700 mt-2">--}}
+                    {{--Update your README.md file to include our Treeware "conditions of use" text. This will provide people with a way to buy trees for the Treeware Forrest on Offset.Earth.--}}
+                {{--</p>--}}
+            {{--</div>--}}
+
+            {{--<div class="w-full bg-white shadow-lg rounded-lg px-4 py-6 lg:p-8 md:mx-2 lg:mx-4 mt-4 md:mt-0">--}}
+                {{--<img src="images/icon-home-3.svg" alt="" class="mx-auto h-32">--}}
+                {{--<h4 class="text-xl font-bold leading-tight mt-8">3. Watch our forrest grow!</h4>--}}
+                {{--<p class="text-gray-700 mt-2">--}}
+                    {{--When people use your package we hope they will support you by buying the world a few trees. In turn you’ll help the highest standard climate projects to accelerate our transition to a sustainable future.--}}
+                {{--</p>--}}
+            {{--</div>--}}
+        </div>
+
+        <div class="mt-12 md:mt-20 w-full md:max-w-3xl mx-auto">
+            <p class="text-xl">&ldquo;I've seen a big uplift in people adding methods of accepting payments to their opensource packages. I think there is a place for this but it didn’t sit well for me and for the small packages I have released. I loved the idea of Postcardware but it still didn’t feel right for me. When I read about Phil Sturgeon work with Offset.Earth I knew this was exactly the right fit for me and Treeware was born.&rdquo;</p>
+            <p class="text-xl font-bold text-green-700 mt-4">James Mills</p>
+            <p class="text-sm uppercase tracking-wider text-gray-700">Treeware.Earth</p>
+        </div>
+    </section>
+    <!-- end how it works -->
+
+    <!-- start features -->
+    <section class="relative bg-green-600 px-4 sm:px-8 lg:px-16 xl:px-40 2xl:px-64 pt-20 pb-40">
+        <div class="text-center">
+            <h2 class="text-3xl leading-tight font-bold text-white">The Treeware "conditions of use"</h2>
+        </div>
+
+        <div class="w-full text-center">
+            <div class="py-6 text-white">
+                <p>Below is an sample "conditions of use" which you are free to use in your projects.</p>
+            </div>
+            <script src="https://gist.github.com/jamesmills/add9a716c17628494e58f684b2615c66.js"></script>
+        </div>
+
+        <div class="w-full text-center">
+            <div class="py-6 text-white">
+                <p>Below is the Treeware badge which you can use in your project.</p>
+            </div>
+            <script src="https://gist.github.com/jamesmills/44504132a6434ee4d00fe66357fec94b.js"></script>
+        </div>
+    </section>
+    <!-- end features -->
+
+    <!-- start cta -->
+    {{--<section class="relative px-4 sm:px-8 lg:px-16 xl:px-40 2xl:px-64 py-12 text-center md:text-left -mt-32">--}}
+        {{--<div class="flex flex-col md:flex-row items-center justify-between bg-blue-300 px-12 py-10 rounded-lg shadow-lg">--}}
+            {{--<div class="w-full md:w-2/3">--}}
+                {{--<h2 class="text-3xl leading-tight font-bold">Need help?</h2>--}}
+                {{--<p class="mt-2 md:max-w-md">Contact our Customer Support that is always ready to help you with any possible questions, problems or--}}
+                    {{--information.</p>--}}
+            {{--</div>--}}
+
+            {{--<div class="w-full md:w-1/3 md:text-right mt-6 md:mt-0">--}}
+                {{--<a href="#" class="inline-block px-6 py-4 bg-blue-600 text-white rounded-lg">Go to Support</a>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</section>--}}
+    <!-- end cta -->
+
+    <!-- start footer -->
+    <footer class="relative bg-white px-4 sm:px-8 lg:px-16 xl:px-40 2xl:px-64 pt-12 pb-10 text-center sm:text-left">
+        <div class="flex flex-col sm:flex-row sm:flex-wrap">
+            <div class="sm:w-1/2 lg:w-1/3">
+                <h6 class="text-sm text-gray-600 font-bold uppercase">Resources</h6>
+                <ul class="mt-4">
+                    <li><a href="https://gist.github.com/jamesmills/add9a716c17628494e58f684b2615c66">Treeware licence example</a></li>
+                    <li class="mt-2"><a href="https://gist.github.com/jamesmills/44504132a6434ee4d00fe66357fec94b">Treeware badge</a></li>
+                </ul>
+            </div>
+
+            <div class="mt-8 sm:w-1/2 sm:mt-0 lg:w-1/3 lg:mt-0">
+                <h6 class="text-sm text-gray-600 font-bold uppercase">Inspiration</h6>
+                <ul class="mt-4">
+                    <li>
+                        <a href="https://twitter.com/freekmurze">Freek</a>, <a href="https://freek.dev/531-our-packages-are-now-postcardware">Spatie Postcardware</a>
+                    </li>
+                    <li class="mt-2">
+                            <a href="https://twitter.com/philsturgeon">Phil Sturgeon</a>, <a href="https://protect.earth/">Protect Earth</a>
+                    </li>
+                    <li class="mt-2">
+                        <a href="http://offset.earth/">Offset Earth</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="mt-8 sm:w-1/2 sm:mt-0 lg:w-1/3 lg:mt-0">
+                <h6 class="text-sm text-gray-600 font-bold uppercase">Thanks</h6>
+                <ul class="mt-4">
+                    <li>
+                        Photos by <a href="https://unsplash.com/@davidvig?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">David Vig</a> on<a href="https://unsplash.com/s/photos/tree?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+                        D. Jameson RAGE
+                        https://unsplash.com/@rage_devil_jameson?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText
+
+                        Kyle Glenn
+                        https://unsplash.com/@kylejglenn?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText
+                    </li>
+                    <li class="mt-2">
+                        <a href="https://www.vecteezy.com/">Tree Illustration by Vecteezy</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="mt-16">
+            <hr class="mb-8">
+            <p class="text-sm text-gray-600">2020 © Treeware. All rights reserved.</p>
+        </div>
+    </footer>
+    <!-- end footer -->
+
+</main>
+
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+
+<script>
+    $('.navbar-toggler').click(function () {
+        $(this).toggleClass('active');
+        $('.navigation-menu').toggleClass('hidden');
+        $('#navbar').addClass('bg-white');
+    });
+
+    $(function () {
+        var navigation = $("#navbar");
+
+        $(window).scroll(function () {
+            var scroll = $(window).scrollTop();
+            if (scroll >= 10) {
+                navigation.addClass("bg-white xl:py-4 shadow-md");
+                navigation.removeClass("xl:py-8");
+            } else {
+                navigation.removeClass("bg-white xl:py-4 shadow-md");
+                navigation.addClass("xl:py-8");
+            }
+        });
+    });
+</script>
+
+
+</body>
+
 </html>
