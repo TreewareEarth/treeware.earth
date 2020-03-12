3c051ff936d695206c457b0cf34a5a6106567044
James Mills <james@jamesmills.co.uk>
2020-03-12 09:15:33 +0400

    Removing the 404 page and always redirecting

--- app/Http/Controllers/ClickController.php
+++ app/Http/Controllers/ClickController.php
@@ -12,37 +12,33 @@ class ClickController extends Controller
     public function index(Request $request)
     {
 
-        try {
+        $hash = md5($request->route('owner') . $request->route('package_name'));
 
-
-            $package = Package::query()
-                ->whereOwner($request->route('owner'))
-                ->wherePackageName($request->route('package_name'))
-                ->firstOrFail();
-
-        } catch (ModelNotFoundException $e) {
-            return view('notfound', [
-                'owner' => $request->route('owner'),
-                'package_name' => $request->route('package_name'),
-            ]);
-        }
+        $package = Package::query()
+            ->whereOwner($request->route('owner'))
+            ->wherePackageName($request->route('package_name'))
+            ->firstOrFail();
 
         $geoIp = geoip()->getLocation($request->server->get('REMOTE_ADDR'));
 
-        $package->clicks()->create([
-            'ip' => $request->server->get('REMOTE_ADDR'),
-            'country_iso_code' => $geoIp['iso_code'],
-            'country' => $geoIp['country'],
-            'city' => Str::limit($geoIp['city'], 57),
-            'state' => $geoIp['state'],
-            'state_name' => $geoIp['state_name'],
-            'postal_code' => $geoIp['postal_code'],
-            'latitude' => $geoIp['lat'],
-            'longitude' => $geoIp['lon'],
-            'timezone' => $geoIp['timezone']
-        ]);
+        if ($package) {
+            $package->clicks()->create([
+                'ip' => $request->server->get('REMOTE_ADDR'),
+                'country_iso_code' => $geoIp['iso_code'],
+                'country' => $geoIp['country'],
+                'city' => Str::limit($geoIp['city'], 57),
+                'state' => $geoIp['state'],
+                'state_name' => $geoIp['state_name'],
+                'postal_code' => $geoIp['postal_code'],
+                'latitude' => $geoIp['lat'],
+                'longitude' => $geoIp['lon'],
+                'timezone' => $geoIp['timezone']
+            ]);
+        }
 
-        $hash = md5($package->owner . $package->package_name);
+        // TODO if NO $package then email James to tell him
+
+        // TODO if NO $package then lets' run a check to see if the package does have Treeware then we can auto add it
 
         return redirect()->to('https://offset.earth/treeware?gift-trees&ref=' . $hash);
 
