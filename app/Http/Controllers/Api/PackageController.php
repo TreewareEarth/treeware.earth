320ea5e3f1cae4cd79f69f34f075e2da024f5bf2
James Mills <james@jamesmills.co.uk>
2020-03-19 10:52:08 +0000

    Creating a job to check if a missing package can be added when someone clicks the plant link

--- app/Http/Controllers/Api/PackageController.php
+++ app/Http/Controllers/Api/PackageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Actions\Package\CreatePackageAction;
 use App\Events\Package\NewPackageCreated;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Package\ValidatePackageRequest;
@@ -24,29 +25,7 @@ class PackageController extends Controller
 
     public function store(ValidatePackageRequest $request)
     {
-
-        $github_info = (new PackageExplorer())->getGitHubRepoInfo($request->get('package_url'));
-
-        if ($content = (new PackageExplorer())->getJsonFileContent($request->get('package_url'))) {
-            $description = $content['description'];
-        } elseif ($content = (new PackageExplorer())->getJsonFileContent($request->get('package_url'), 'package')) {
-            $description = $content['description'];
-        } else {
-            $description = $github_info['description'];
-        }
-
-        $package = Package::create([
-            'owner' => $github_info['owner']['login'],
-            'package_name' => $github_info['name'],
-            'owner_avatar_url' => $github_info['owner']['avatar_url'],
-            'description' => $description,
-            'package_url' => $request->get('package_url')
-        ]);
-
-        event(new NewPackageCreated($package));
-
-        return $package;
-
+        return (new CreatePackageAction())->execute($request->get('package_url'));
     }
 
 }
