e0b3d676e401b1a84d8d46e99f5378f001b4b3d7
Furqan Freed <furqan@clicksco.com>
2020-12-14 11:38:08 +0400

    search component seprated from global js and separetely load on search page
    our search page use algolia and for production webpack compile it require production credentials.

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -68,6 +68,7 @@
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 
 <script src="{{ mix('/js/app.js') }}"></script>
+@yield('script')
 
 <script>
 
