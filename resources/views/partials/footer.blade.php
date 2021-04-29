9e9d2489840fe8b5f78731278762a27d08b03ea6
Oliver Stark <os@fortrabbit.com>
2021-04-29 16:43:07 +0200

    Global footer + dynamic copyright year

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,42 @@
+<!-- start footer -->
+<footer class="relative bg-white px-4 sm:px-8 lg:px-16 xl:px-40 2xl:px-64 pt-12 pb-10 text-center sm:text-left">
+    <div class="flex flex-col sm:flex-row sm:flex-wrap">
+        <div class="sm:w-1/2 lg:w-1/3">
+            <h6 class="text-sm text-gray-600 font-bold uppercase">Resources</h6>
+            <ul class="mt-4">
+                <li><a href="https://gist.github.com/jamesmills/add9a716c17628494e58f684b2615c66">Treeware licence example</a></li>
+                <li class="mt-2"><a href="https://gist.github.com/jamesmills/44504132a6434ee4d00fe66357fec94b">Treeware badge</a></li>
+            </ul>
+        </div>
+
+        <div class="mt-8 sm:w-1/2 sm:mt-0 lg:w-1/3 lg:mt-0">
+            <h6 class="text-sm text-gray-600 font-bold uppercase">Inspiration</h6>
+            <ul class="mt-4">
+                <li>
+                    <a href="https://twitter.com/freekmurze">Freek</a>, <a href="https://freek.dev/531-our-packages-are-now-postcardware">Spatie Postcardware</a>
+                </li>
+                <li class="mt-2">
+                    <a href="https://twitter.com/philsturgeon">Phil Sturgeon</a>, <a href="https://protect.earth/">Protect Earth</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="mt-8 sm:w-1/2 sm:mt-0 lg:w-1/3 lg:mt-0">
+            <h6 class="text-sm text-gray-600 font-bold uppercase">Thanks</h6>
+            <ul class="mt-4">
+                <li class="mt-2">
+                    <a href="https://www.vecteezy.com/">Tree Illustration by Vecteezy</a>
+                </li>
+                <li class="mt-2">
+                    <a href="http://ecologi.com/treeware">Ecologi</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="mt-16">
+        <hr class="mb-8">
+        <p class="text-sm text-gray-600">{{ date('Y') }} © Treeware. All rights reserved.</p>
+    </div>
+</footer>
+<!-- end footer -->
