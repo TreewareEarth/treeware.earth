1527d5969d3926208699cdea0deb65faf0e4999f
Oliver Stark <os@fortrabbit.com>
2021-04-30 15:36:11 +0200

    Error prove 'description' access

--- app/Services/Github/PackageExplorer.php
+++ app/Services/Github/PackageExplorer.php
@@ -9,68 +9,58 @@ use Illuminate\Support\Arr;
 
 class PackageExplorer
 {
-    public function findTextInReadme($url, $text = 'treeware')
-    {
-        $response = (new Client())->request(
-            'GET',
-            'https://raw.githubusercontent.com' . (parse_url($url)['path']) . '/' . $this->getDefaultBranch($url) . '/README.md'
-        );
+    public const GITHUB_RAW_DOMAIN = 'https://raw.githubusercontent.com';
+    public const GITHUB_API_DOMAIN = 'https://api.github.com';
 
-        return strpos(strtolower(($response->getBody())->getContents()), $text) == true;
+
+    public function hasGithubReadmeFile($url): bool
+    {
+        return ($this->getContent($this->getGithubRawUrlToFile($url, 'README.md')));
     }
 
-    public function getJsonFileContent($url, $filename = 'composer.json')
+    public function findTextInReadme($url, $text = 'treeware'): bool
+    {
+        $content = $this->getContent($this->getGithubRawUrlToFile($url, 'README.md'));
+
+        return strpos(strtolower($content), $text) !== false;
+    }
+
+    public function parseJsonFile($url, $filename = 'composer.json'):? array
+    {
+        $content = $this->getContent($this->getGithubRawUrlToFile($url, $filename));
+
+        return ($content) ? json_decode($content, true) : null;
+    }
+
+    public function getGitHubRepoInfo($url):? array
+    {
+        $path = ltrim(parse_url($url)['path'], '/');
+        $fullUrl = sprintf('%s/repos/%s', self::GITHUB_API_DOMAIN, $path);
+        $content = $this->getContent($fullUrl);
+
+        return ($content) ? json_decode($content, true) : null;
+    }
+
+    private function getGithubRawUrlToFile($url, $filename): string
+    {
+        $path = ltrim(parse_url($url)['path'], '/');
+        $defaultBranch = Arr::get($this->getGitHubRepoInfo($url), 'default_branch', 'master');
+
+        return sprintf('%s/%s/%s/%s', self::GITHUB_RAW_DOMAIN, $path, $defaultBranch, $filename);
+    }
+
+    private function getContent($fullUrl):? string
     {
         try {
-            $response = (new Client())->request(
-                'GET',
-                'https://raw.githubusercontent.com' . (parse_url($url)['path']) . '/' . $this->getDefaultBranch($url) . '/' . $filename,
-                [
-                    'stream' => true,
-                ]
-            );
-        } catch (ClientException $e) {
+            $response = (new Client())->get($fullUrl);
+        } catch (\Exception $e) {
             return null;
         }
 
-
-        return json_decode($response->getBody()->getContents(), true);
-    }
-
-    public function getGitHubRepoInfo($url)
-    {
-        $response = (new Client())->request(
-            'GET',
-            'https://api.github.com/repos' . (parse_url($url)['path']),
-            [
-                'stream' => true,
-            ]
-        );
-
-        return json_decode($response->getBody()->getContents(), true);
-    }
-
-    public function hasGithubReadmeFile($url)
-    {
-        try {
-            return (new Client())->request(
-                'GET',
-                'https://raw.githubusercontent.com' . (parse_url($url)['path']) . '/master/README.md',
-                [
-                    'stream' => true,
-                ]
-            );
-        } catch (ClientException $e) {
-            return false;
-        } catch (\Exception $e) {
-            return false;
+        if ($response->getStatusCode() >= 400) {
+            return null;
         }
-    }
 
-    public function getDefaultBranch($url)
-    {
-        $reportData = $this->getGitHubRepoInfo($url);
-
-        return Arr::get($reportData, 'default_branch', 'master');
+        return $response->getBody()->getContents();
     }
 }
