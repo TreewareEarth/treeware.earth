380ba484f84d4485ae6a3bca74830325f52d6ebd
Oliver Stark <os@fortrabbit.com>
2021-05-06 13:48:59 +0200

    prepare package landing page

--- app/Http/Controllers/ClickController.php
+++ app/Http/Controllers/ClickController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Jobs\Package\CreatePackageIfHasLicence;
 use App\Package;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
@@ -12,7 +11,7 @@ class ClickController extends Controller
 {
     public function index(Request $request)
     {
-        $owner = $request->route('owner', 'empty');
+        $owner        = $request->route('owner', 'empty');
         $package_name = $request->route('package_name', 'empty');
 
         $package = Package::query()
@@ -21,25 +20,54 @@ class ClickController extends Controller
             ->first();
 
 
-        if ($package) {
-            $geoIp = geoip()->getLocation($request->server->get('REMOTE_ADDR'));
-            $package->clicks()->create([
-                'ip' => $request->server->get('REMOTE_ADDR'),
-                'country_iso_code' => $geoIp['iso_code'],
-                'country' => $geoIp['country'],
-                'city' => Str::limit($geoIp['city'], 57),
-                'state' => $geoIp['state'],
-                'state_name' => $geoIp['state_name'],
-                'postal_code' => $geoIp['postal_code'],
-                'latitude' => $geoIp['lat'],
-                'longitude' => $geoIp['lon'],
-                'timezone' => $geoIp['timezone'],
-            ]);
+        if ($package instanceof Package) {
 
-            return redirect()->to('https://ecologi.com/treeware?gift-trees&ref=' . md5(Str::lower($owner . '/' . $package_name)));
+            $this->trackClick($package, $request);
+
+            return view('plant')->with([
+                'package'      => $package,
+                'checkout_url' => $this->getCheckoutUrl($package),
+            ]);
         }
 
         throw new NotFoundHttpException("The package '$owner/$package_name' is not registered with treeware.earth");
+    }
+
+    private function getCheckoutUrl(Package $package): string
+    {
+        $ref = md5(Str::lower($package->owner . '/' . $package->package_name));
+
+        return 'https://ecologi.com/treeware?gift-trees&ref=' . $ref;
+    }
+
+    private function trackClick(Package $package, Request $request): bool
+    {
+        $remoteAddr = $request->server->get('REMOTE_ADDR');
+        $cacheKey = md5($package->package_name . $remoteAddr);
+
+        // Prevent click cheating
+        if (cache()->has($cacheKey)) {
+            return false;
+        }
+
+        $geoIp = geoip()->getLocation($remoteAddr);
+        $package->clicks()->create([
+            'ip'               => $remoteAddr,
+            'country_iso_code' => $geoIp['iso_code'],
+            'country'          => $geoIp['country'],
+            'city'             => Str::limit($geoIp['city'], 57),
+            'state'            => $geoIp['state'],
+            'state_name'       => $geoIp['state_name'],
+            'postal_code'      => $geoIp['postal_code'],
+            'latitude'         => $geoIp['lat'],
+            'longitude'        => $geoIp['lon'],
+            'timezone'         => $geoIp['timezone'],
+        ]);
+
+        // Remember the click
+        cache()->set($cacheKey, '1', 600);
+
+        return true;
 
     }
 }
